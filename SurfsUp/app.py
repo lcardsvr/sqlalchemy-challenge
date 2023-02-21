2c0079530b41b6f724b3e964b8b635fc894d8435
Luis Cardenas <lcardsvr@gmail.com>
2023-02-21 19:05:17 +0800

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -111,27 +111,26 @@ def precipitation():
 
 
 
-# @app.route("/api/v1.0/stations")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    # stations_grp= session.query(Measurement).group_by(Measurement.station).order_by(func.co).all()
 
-#     session.close()
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
+    stations_grp= session.query(Measurement).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
 
-#     return jsonify(all_passengers)
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    stations = []
+    for station in stations_grp:
+        station_dict = {}
+        station_dict["station"] = station
+        stations.append(station_dict)
+
+    return jsonify(stations)
 
 
 # @app.route("/api/v1.0/tobs")
